7bf81f1a3c58b4ba8e817a60b54d4b5aab64bba1
Paszta <63753108+Paszta@users.noreply.github.com>
2023-05-29 22:26:34 +0200

    Zadanie 1

---
+++ sprawozdanie1/spr1/src/main/java/com/example/spr1/Spr1Application.java
@@ -0,0 +1,64 @@
+package com.example.spr1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@RestController
+public class Spr1Application {
+
+	String hostAddress;
+	String tz;
+	static Properties properties = new Properties();
+
+
+	@RequestMapping("/")
+	public String home(TimeZone timeZone1) throws UnknownHostException {
+		// adres IP odczytany z własności klasy InetAddress - metoda: getLocalHost i getHostAddress pozwala na wyciągnięcie obiektu hosta i odczytanie jego adresu IP
+		hostAddress = InetAddress.getLocalHost().getHostAddress();
+		// wyswietlenie czasu i strefy czasowej na podstawie pobranego z klasy TimeZone id strefy
+		tz = ZonedDateTime.now(ZoneId.of(timeZone1.getID())).toString();
+		return " adres IP: " + hostAddress + " timezone: " + tz;
+	}
+
+
+	public static void main(String[] args) {
+		//inicjalizacja zmiennych
+		String appPort = "";
+		String date;
+		String nameSurname;
+		SpringApplication.run(Spr1Application.class, args);
+
+		// numer portu ustawiony jest w application.properties
+		//poniższy kod odcztywuje jego wartosc z pliku, z własności o nazwie server.port
+		try {
+			properties.load(Spr1Application.class.getClassLoader().getResourceAsStream("application.properties"));
+			appPort = properties.getProperty("server.port").toString();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		// czas uruchomienia
+		date = LocalTime.now().toString();
+		nameSurname = "Alicja Pasztelan";
+		//wyswietlenie wartosci
+		System.out.println("Port: " + appPort + " data: " + date + " imie i nazwisko: " + nameSurname);
+	}
+
+}
